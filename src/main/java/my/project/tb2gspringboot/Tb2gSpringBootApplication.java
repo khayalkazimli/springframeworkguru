3fb48d8d9daa77dc37c6a189bf7b3c1251332137
Khayal Kazimli <khayalkazimli@gmail.com>
2022-01-18 21:07:55 +0400

    added circleci config

---
+++ src/main/java/my/project/tb2gspringboot/Tb2gSpringBootApplication.java
@@ -0,0 +1,13 @@
+package my.project.tb2gspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tb2gSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tb2gSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/project/tb2gspringboot/Tb2gSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.tb2gspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tb2gSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
